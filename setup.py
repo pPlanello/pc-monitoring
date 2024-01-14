d429a39f39d7ba7bb5ad874bf2c75efdab50dbc2
pplanello <pplanellodev@gmail.com>
2024-01-14 21:08:56 +0100

    Refactor code from file service

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name="pc-monitoring",
-    version="1.2.0",
+    version="1.2.1",
     install_requires=["psutil"],
 )
--- src/file_service.py
+++ src/file_service.py
@@ -6,15 +6,18 @@ __PATH = 'monitoring'
 
 def save_data_logs_in_file(date: datetime, message: str):
     date_day = date.strftime('%Y-%m-%d')
-    file_name = 'data_monitoring_from_' + date_day + '.txt'
-
-    full_path = os.path.join(__get_root_dir(), __PATH, date_day, file_name)
+    full_path = __get_full_path(date_day)
 
     # Create directory if it does not exist
     os.makedirs(os.path.dirname(full_path), exist_ok=True)
 
     with open(full_path, 'a') as file:
         file.write(message + '\n')
+
+
+def __get_full_path(date: str):
+    file_name = 'data_monitoring_from_' + date + '.txt'
+    return os.path.join(__get_root_dir(), __PATH, date, file_name)
 
 
 def __get_root_dir():
